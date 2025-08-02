8f4f4e2bcb66d14e36b3593e9430cfe6948a1bea
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-08-02 23:33:09 +0530

    added a file

---
+++ OOPs/Inheritance/Shape.java
@@ -0,0 +1,18 @@
+public class Shape{
+private int borderWidth;
+private String color;
+
+public void setBorderWidth(int borderWidth){
+this.borderWidth=borderWidth;
+}
+public int getBorderWidth(){
+return borderWidth;
+}
+
+public void setColor(String color){
+this.color=color;
+}
+public String getColor(){
+return color;
+}
+}
