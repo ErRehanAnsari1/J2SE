b067cf996acbbb0c36bedb9f8f2717fc2ddad02e
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-08-04 23:37:38 +0530

    add a file

---
+++ OOPs/Inheritance/Staff3.java
@@ -0,0 +1,18 @@
+public class Staff3 extends Person3{
+private int staffID;
+private int salary;
+
+public void setStaffID(int staffID){
+this.staffID=staffID;
+}
+public int getStaffID(){
+return staffID;
+}
+
+public void setSalary(int salary){
+this.salary=salary;
+}
+public int getSalary(){
+return salary;
+}
+}
