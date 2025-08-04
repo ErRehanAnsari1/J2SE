1f4cde2f23547b310fd92701b0dc5e8a3426e719
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-08-04 23:38:31 +0530

    added a file

---
+++ OOPs/Inheritance/Student3.java
@@ -0,0 +1,26 @@
+public class Student3 extends Person3{
+private int rollNumber;
+private int standard;
+private double fees;
+
+public void setRollNumber(int rollNumber){
+this.rollNumber=rollNumber;
+}
+public int getRollNumber(){
+return rollNumber;
+}
+
+public void setStandard(int standard){
+this.standard=standard;
+}
+public int getStandard(){
+return standard;
+}
+
+public void setFees(double fees){
+this.fees=fees;
+}
+public double getFees(){
+return fees;
+}
+}
