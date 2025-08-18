140a94215026972b5a6f4d8bfc9068d4b7731486
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-08-18 23:15:35 +0530

    added a file

---
+++ OOPs/Inheritance/Person.java
@@ -0,0 +1,28 @@
+public class Person{
+private String name;
+private String address;
+
+public Person(){
+this.name="Rehan Ansari";
+}
+
+public Person(String name,String address){
+this.name=name;
+this.address=address;
+}
+
+public void setName(String name){
+this.name=name;
+}
+public String getName(){
+return name;
+}
+
+public void setAddress(String address){
+this.address=address;
+}
+public String getAddress(){
+return address;
+}
+
+}
