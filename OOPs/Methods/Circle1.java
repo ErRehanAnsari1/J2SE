2325e7aa50e5cfec2319c455b070c82c47637b41
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-09-01 23:14:11 +0530

    new file

---
+++ OOPs/Methods/Circle1.java
@@ -0,0 +1,35 @@
+public class Circle{
+private int borderWidth=0;
+private String color=null;
+private double radius=0.0;
+
+public void setBorderWidth(int borderWidth){
+
+this.borderWidth=borderWidth;
+}
+public int getBorderWidth(){
+return borderWidth;
+}
+
+
+public void setColor(String color){
+this.color=color;
+}
+public String getColor(){
+return color;
+}
+
+public void setRadius{
+this.radius=radius;
+}
+public double getRadius(){
+return radius;
+}
+
+public double area(){
+return 3.14*rd*rd;
+}
+public double circumference(){
+return 2*3.14*rd;
+}
+}
