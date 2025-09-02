4f7d578255ba6c6bc934662a2dc2910018303207
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-09-02 22:37:51 +0530

    added new file

---
+++ OOPs/Methods/Movie.java
@@ -0,0 +1,34 @@
+public class Movie{
+private String title;
+private String genre;
+private int duration;
+private double rating;
+
+public void setTitle(String title){
+this.title=title;
+}
+public String getTitle(){
+return title;
+}
+
+public void setGenre(String genre){
+this.genre=genre;
+}
+public String getGenre(){
+return genre;
+}
+
+public void setDuration(int duration){
+this.duration=duration;
+}
+public int getDuration(){
+return duration;
+}
+
+public void setRating(double rating){
+this.rating=rating;
+}
+public double getRating(){
+return rating;
+}
+}
