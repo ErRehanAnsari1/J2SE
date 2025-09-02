3a2350af4a29d4320b2d985796b8505853a887ab
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-09-02 22:36:44 +0530

    added new file

---
+++ OOPs/Methods/Furniture.java
@@ -0,0 +1,34 @@
+public class Furniture{
+private String chair;
+private String material;
+private int price;
+private double weight;
+
+public void setChair(String chair){
+this.chair=chair;
+}
+public String getChair(){
+return chair;
+}
+
+public void setMaterial(String material){
+this.material=material;
+}
+public String getMaterial(){
+return material;
+}
+
+public void setPrice(int price){
+this.price=price;
+}
+public int getPrice(){
+return price;
+}
+
+public void setWeight(double weight){
+this.weight=weight;
+}
+public double getWeight(){
+return weight;
+}
+}
