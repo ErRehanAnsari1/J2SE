4600d8a2e55fc9c46af71b1369001fba4ea872e8
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-09-04 23:21:15 +0530

    new file

---
+++ OOPs/Methods/SetterGetter1.java
@@ -0,0 +1,20 @@
+public class SetterGetter1{
+private int borderWidth;
+private String color;
+
+
+public void setBorderWidth(int borderWidth){
+this.borderWidth=borderWidth;
+}
+public int getBorderWidth(){
+return borderWidth;
+}
+
+public void setColor(String color){
+this.color=color;
+}
+public String getColor(){
+return color;
+}
+ 
+}
