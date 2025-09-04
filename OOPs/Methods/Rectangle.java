f5796bf49898e9786437d3361c4182277ca00acf
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-09-04 23:22:22 +0530

    new file

---
+++ OOPs/Methods/Rectangle.java
@@ -0,0 +1,41 @@
+public class Rectangle{
+
+private int borderWidth=0;
+private String color="null";
+private int width=0;
+private int length=0;
+
+public void setBorderWidth(int borderWidth){
+this.borderWidth=borderWidth;
+}
+public int getBorderWidth(){
+return borderWidth;
+}
+public void setColor(String color){
+this.color=color;
+}
+public String getColor(){
+return color;
+}
+public void setLength(int length){
+this.length=length;
+}
+public int getLength(){
+return length;
+}
+public void setWidth(int width){
+this.width=width;
+}
+public int getWidth(){
+return width;
+}
+
+public int area(){
+return length*width;
+}
+public int perimeter(){
+return (length+width)*2;
+}
+
+
+}
