5f1a20708a7c077ef5e389f7f61071b94b05ae8b
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-10-02 22:53:55 +0530

    added new file

---
+++ OOPs/Abstraction/Printer.java
@@ -0,0 +1,27 @@
+interface Printer{
+
+default void printHello(){
+commonHeader();
+System.out.println("Hello world");
+}
+
+default void printBye(){
+commonHeader();
+System.out.println("GoodBye");
+}
+
+private void commonHeader(){
+System.out.println("----printing start----");
+}
+}
+class MyPrinter implements Printer{}
+
+public class TestPrinter{
+public static void main(String[] args){
+
+Printer P=new MyPrinter();
+p.printHello();
+p.printBye();
+
+}
+}
