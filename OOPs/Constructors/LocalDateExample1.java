878bf9e33c2a7a5c19b3b21a897b55f3c3f05175
Er Rehan Ansari <rehanansaridard@gmail.com>
2026-01-10 12:40:15 +0530

    added new file

---
+++ OOPs/Constructors/LocalDateExample1.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class LocalDateExample1{
+public static void main(String[] args){
+LocalDate dob=LocalDate.of(2003,6,16);
+LocalDate today=LocalDate.now();
+
+Period age=Period.between(dob,today);
+System.out.println("Age : "+age.getYears()+" years");
+}
+}
